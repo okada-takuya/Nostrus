ad881d95debf1a7cb51c54cab6f2a3407f0fcc9a
okada-takuya <pitipitiunsyumikan@gmail.com>
2024-01-14 22:54:48 +0900

    Metadata 1件を取得可能にした

--- src/app/infrastructures/nostr-api.service.ts
+++ src/app/infrastructures/nostr-api.service.ts
@@ -13,6 +13,7 @@ import { Event } from 'nostr-typedef';
 import { Observable, ReplaySubject, bufferTime } from 'rxjs';
 
 import { EventStoreService } from '../shared/event-store.service';
+import { Kind } from '../domain/kind';
 
 @Injectable({
   providedIn: 'root',
@@ -25,6 +26,9 @@ export class NostrApiService {
   readonly eventPackets$: Observable<EventPacket>;
   readonly metadetaPackets$: Observable<EventPacket>;
 
+  private metadataReq = createRxBackwardReq();
+  private eventsReq = createRxForwardReq();
+
   constructor(private eventStoreService: EventStoreService) {
     const allMessage$ = this.rxNostr.createAllMessageObservable();
     allMessage$.pipe(filterByType('NOTICE')).subscribe((packet) => {
@@ -34,17 +38,23 @@ export class NostrApiService {
       console.error('[rx-nostr closed]', packet);
     });
 
-    const metadataReq = createRxBackwardReq();
-    const eventsReq = createRxForwardReq();
-
     this.eventPackets$ = this.rxNostr
-      .use(eventsReq.pipe(bufferTime(1000, null, 10), batch()))
+      .use(this.eventsReq.pipe(bufferTime(1000, null, 10), batch()))
       // TODO: nostterのようにEventの参照イベントのリクエストはtapで投げたほうが良いと思われる
       // Refers: https://github.com/SnowCait/nostter/blob/2efe229a5c2388e5b12e77cd6a86dd24b5b1236c/web/src/lib/timelines/MainTimeline.ts#L82
       .pipe(uniq());
 
     this.metadetaPackets$ = this.rxNostr.use(
-      metadataReq.pipe(bufferTime(1000, null, 10), batch())
+      this.metadataReq.pipe(bufferTime(1000, null, 10), batch())
     );
+  }
+
+  emitMetadataReq(pubkey: string) {
+    console.debug('[rx-nostr metadata REQ emit]', pubkey);
+    this.metadataReq.emit({
+      kinds: [Kind.Metadata],
+      authors: [pubkey],
+      limit: 1,
+    });
   }
 }
--- src/app/shared/metadata-store.service.ts
+++ src/app/shared/metadata-store.service.ts
@@ -13,27 +13,36 @@ export class MetadataStoreService {
     string,
     Content.Metadata
   >();
-  readonly incomingMetadata$: ReplaySubject<Content.Metadata> = new ReplaySubject();
+  readonly incomingMetadata$: ReplaySubject<Content.Metadata> =
+    new ReplaySubject();
 
   constructor(private nostrApiService: NostrApiService) {
-    this.nostrApiService.metadetaPackets$.subscribe(
-      (packet: EventPacket) => {
-        const event: Event = packet.event;
-        const metadata: Content.Metadata = JSON.parse(event.content);;
-        this.eventMap.set(event.pubkey, metadata);
-        this.incomingMetadata$.next(metadata);
-      }
-    );
+    this.nostrApiService.metadetaPackets$.subscribe((packet: EventPacket) => {
+      const event: Event = packet.event;
+      const metadata: Content.Metadata = JSON.parse(event.content);
+      this.eventMap.set(event.pubkey, metadata);
+      this.incomingMetadata$.next(metadata);
+    });
   }
 
   observeMetadataOf(pubkey: string): Observable<Content.Metadata> {
     // Mapから取得可能だった場合に、通常のSubjectでは再配信不可能なため。
-    const metadata$: ReplaySubject<Content.Metadata> = new ReplaySubject<Content.Metadata>();
+    const metadata$: ReplaySubject<Content.Metadata> =
+      new ReplaySubject<Content.Metadata>();
     const metadata: Content.Metadata | undefined = this.eventMap.get(pubkey);
     if (metadata) {
       metadata$.next(metadata);
     } else {
-      // TODO metadata 1件読みのNostrAPIへのemit->subscribeしてsubjectに流す
+      const subscription = this.nostrApiService.metadetaPackets$.subscribe(
+        (packet: EventPacket) => {
+          const event: Event = packet.event;
+          const metadata: Content.Metadata = JSON.parse(event.content);
+          metadata$.next(metadata);
+          // 1件でsubscriptionを終わってよい。
+          subscription.unsubscribe;
+        }
+      );
+      this.nostrApiService.emitMetadataReq(pubkey);
     }
     return metadata$;
   }
